ccb36d82e170355ef84adb7766cc14bee98ce4c2
cheezi0747 <linus.sjolinder@b3.se>
2024-12-10 10:56:50 +0100

    UF-11274: Fix deperacted mockbean

--- src/test/java/se/sundsvall/smssender/api/SmsResourceTests.java
+++ src/test/java/se/sundsvall/smssender/api/SmsResourceTests.java
@@ -15,7 +15,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.zalando.problem.violations.ConstraintViolationProblem;
@@ -36,7 +36,7 @@ class SmsResourceTests {
 	@Autowired
 	private WebTestClient webTestClient;
 
-	@MockBean
+	@MockitoBean
 	private SmsProviderRouter mockSmsProviderRouter;
 
 	@Captor
